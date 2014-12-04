b832a1240d97068fba47b2027a20ad51c06e4a5f
FidencioYzaguirreIII <fidencioyzaguirre3rd@gmail.com>
2014-12-03 19:18:28 -0600

    Cleaned up repository.
    There is only one main file now.

--- Code/finalProjectMain.java
+++ Code/finalProjectMain.java
@@ -3,133 +3,189 @@ import java.util.*;
 
 public class finalProjectMain {
 
-	public static void main(String[] args) throws IOException{
-
-		menu();
-	}
-	
-	public static void menu() throws IOException
+	public static void main(String[] args) throws IOException
 	{
+		//Starts the menu
+		menu();
+	} 
+	
+	static void menu() throws IOException
+	{
+		//Declaring a scanner input
 		Scanner systemInInput = new Scanner(System.in);
+		
+		//Setting menuInput to blank
 		String menuInput = "";
+		
+		//Setting running to true
 		boolean running = true;
+		
+		//Welcome message
 		System.out.println("Hello, and welcome to the program with more trees then forests!");
 		
+		
+		//Starting the loop, using a label
 		loop:
 		while(running == true)
 		{
-		try{
-			System.out.println("Press 1 to make a tree press 0 to leave this place and never return");
-			menuInput = systemInInput.nextLine();
-			
-			switch(menuInput)
+			//try-catch block for catching errors
+			try
 			{
-			case "1":
-				fileInput();
-				break;
+				//prompting user for input
+				System.out.println();
+				System.out.println("---Choose an Option---");
+				System.out.println("1) Create a new tree");
+				System.out.println("2) Exit the program");
+				System.out.println();
+				System.out.print("Choice: ");
+				menuInput = systemInInput.nextLine();
 				
-			case "0": // need to pay attention and figure out how to end the probram without brekaing everything
-				running = false;
-				continue loop;
-			default: 
-				throw new InputMismatchException();
+				//Using a switch for parsing user input
+				switch(menuInput)
+				{
+					case "1":
+						fileInput();
+						break;
+						
+					case "2": // need to pay attention and figure out how to end the program without breaking everything
+						System.out.println("Goodbye!");
+						System.exit(0);
+					default: 
+						throw new InputMismatchException();
+				}
+				
 			}
-			
-		}
-		catch(InputMismatchException x){ 
-			System.out.println("Invalid input, please try again");
-			continue loop;
-		
-		
-		}
+			catch(InputMismatchException x)
+			{ 
+				//Error message
+				System.out.println("Invalid input, please try again");
+				
+				//Starts the loop from the beginning
+				continue loop;
+			}
 		}
 		systemInInput.close();
 	}
 	
 	public static void fileInput () throws IOException
 	{
-        int lengthOfString = 0, sizeOfList = 0, counterForDuplicates = 0;
+		//Declaring new scanner for opening files
 		Scanner systemInInput = new Scanner(System.in);
-		String fileName = "", word = "", line = "", currentSearchingString ="", currentString = "";
-
-		LinkedList<String> fullString = new LinkedList();
+		
+		//Filename by default is not declared
+		String fileName = null;
+		
+		//Declaring new empty linked list
+		LinkedList<String> fullString = null;
+		
+		//Holds the duplicate name
+		String duplicate = null;
+		
+		
+		
 		boolean fileContinue = true;
+		//The loop will stop when every element has been added
 		loop:
 		while(fileContinue == true)
 		{
-		try{    //getting file name
-                        System.out.println("\nHello, please enter the file you want made into a fabulous tree");
-			fileName = systemInInput.nextLine();
+			//getting file name
+			try
+			{    
+				//Creating the linked list to store everything
+				fullString = new LinkedList<String>();
+				
+				System.out.println();
+	            System.out.println("Please enter the location of the file you want made into a tree (E.g. C:\\folder\\inputFile.txt)");
+	            System.out.print("Choice: ");
+				fileName = systemInInput.nextLine();
+				
+	            //Using Scanner to open file
+	            Scanner fileInput = new Scanner(new FileReader(new File(fileName)));
+	            
+	            //Trying to find the end of the file 
+				while(fileInput.hasNext())
+				{ 
+					
+					
+					//adding the first element to the linked list if it is empty
+					if(fullString.size() == 0)
+					{
+						fullString.addFirst(fileInput.next());
+					}
+					
+					
+					
+					
+					//adding the rest of the elements to the linked list if it is not empty
+					else
+					{
+						//current string being read
+						String string = fileInput.next();
+						
+						
+						//checking every element for duplicates
+						for(int count = 0; count < fullString.size(); count++)
+						{
+							if ((fullString.get(count).compareTo(string)) == 0)
+							{
+								//Saving the duplicate word
+								duplicate = fullString.get(count);
+								
+								//throws only if duplicate is found
+								throw new InputMismatchException();
+							}
+						}
+						
+						
+						//Adding the current word to the end of the list.
+						fullString.addLast(string);
+					}
+				}
+				
+				//Closing the scanner
+				fileInput.close();
+			}
 			
-                       //opening file
-                        BufferedReader fileInput = new BufferedReader(new FileReader(new File(fileName)));
-			while((line = fileInput.readLine()) != null){ //Trying to find the end of the file 
-                            lengthOfString = line.length();
-                            line = line.toLowerCase();
-                            //going char by char to add to a word until a space and then we bump
-                            for(int i = 0; i<lengthOfString;i++)
-                            {   
-                                if((lengthOfString) <= i){
-                                    System.out.println(word);
-                                    fullString.add(word);
-                                    word = "";   
-                                    // throw new InputMismatchException();
-                                }  
-                                else if(line.charAt(i) == ' ')
-                                {
-                                    fullString.add(word);
-                                    word = "";                          
-                                }
-                                else if(line.charAt(i) >= 97 && line.charAt(i) <= 122){
-                                    word += line.charAt(i);
-                                }
-                               
-                            }
-                        
-                        }
-                        
-                        //got to go through and check to make sure there's not repeating words otherwise throw an input mismatch error not working
-			sizeOfList = fullString.size();
-                        for(int i = 0; i<sizeOfList; i++)
-                        {   counterForDuplicates = 0;
-                            currentString = fullString.get(i);
-                            System.out.println(currentString);
-                            for(int y =0; y<sizeOfList;y++)
-                            {
-                                currentSearchingString = fullString.get(y);
-                                 if(fullString.contains(currentString))
-                                 {                                  
-                                     counterForDuplicates +=1;
-                                 }
-                                 if(counterForDuplicates > 1)
-                                 {
-                                     System.out.println("Hey man, how are you");
-                                     System.out.println("I DON'T ALLOW DUPLICATES BACK TO THE BEGGINING, OH HERE'S THE WORD " + currentString);
-                                     continue loop;
-                                 }
-                            }
-                        }
+	        //throws exception whenever a duplicate is found, starts the input loop all over again
+	        catch(InputMismatchException x)
+	        {   
+	           System.out.println("Duplicate word detected: \""+duplicate+"\" - Please enter another file.");
+	           continue loop;
+	        }
+			
+	        catch(NoSuchElementException x)
+	        {
+	           System.out.println("The end of the file was reached succesfully!");
+	        }
+			catch(IOException x)
+			{
+				System.out.println("Error: Could not find file location. Please try again!");
+				continue loop;
+			}	
+			
+			fileContinue = false;
 		}
-                //the idea for this is supposed to throw whenever they put something wrong in. 
-                catch(InputMismatchException x)
-                {   
-                    System.out.println("There was a problem with whatever you put in. I mean...you should probably check what you're putting in the file. \nDon't worry I'm gonna send you back to the part where you put in a file.\n");
-                    continue loop;
-                }
-                catch(NoSuchElementException x)
-                {
-                    System.out.println("The end of the file was reached succesfully");
-                }
-		catch(EOFException x)
+		
+		//Creating the BST
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		
+		//Adding each element to the BST one by one
+		for(int count = 0; count < fullString.size(); count++)
 		{
-			System.out.println("The end of the file has been reached, moving on to the next stage. ");
+			tree.add(fullString.get(count));
 		}
-		catch(IOException x){
-			System.out.println("An error has occured, please try again by inputting the file again");
-                        System.out.println("This was most likely a file not existing");
-			continue loop;
-		}
-			
-		}
-		}
+		
+		
+		tree.preorder(tree.root);		//Creating the preorder string
+		tree.inorder(tree.root);		//Creating the inorder string
+		tree.postorder(tree.root);		//Creating the postorder string
+		
+		//Calling the method to 
+		tree.traversalOrderFiles();
+		
+		
+		System.out.println("Pre Order: "+tree.preorderString);
+		System.out.println("In Order: "+tree.inorderString);
+		System.out.println("Post Order: "+tree.postorderString);
 	}
+}
